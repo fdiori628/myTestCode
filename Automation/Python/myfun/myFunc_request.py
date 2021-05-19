c3b054d5f3c184bcd15c57d8d7a81551fd6e79bd
fdiori <fdiori@vip.qq.com>
2021-05-19 18:05:51 +0800

    update myfunc

--- Automation/Python/myfun/myFunc_request.py
+++ Automation/Python/myfun/myFunc_request.py
@@ -9,7 +9,11 @@ class Myfunc:
     def myfunc_request(self):
         print(self.response.status_code)
 
+    def myfunc_test(self):
+        self.a = "abcde"
+        assert "a" in self.a
+
 
 m = Myfunc()
 
-m.myfunc_request()
+m.myfunc_test()
--- Automation/Python/testCase/test_demo_baidu.py
+++ Automation/Python/testCase/test_demo_baidu.py
@@ -4,16 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
+from myFunc.myFunc_phare1 import webWait
 
 configFile = open('../Resource/test_demo_baidu/testSelenium_config.yaml', 'r')
-config = yaml.load(configFile, Loader=None)
+config = yaml.load(configFile, Loader=yaml.FullLoader)
 
 testdataFile = open('../Resource/test_demo_baidu/testSelenium_Data.yaml', 'r')
-testData = yaml.load(testdataFile, Loader=None)
+testData = yaml.load(testdataFile, Loader=yaml.FullLoader)
 searchData = testData["test_search_data"]
 
 testElementFile = open('../Resource/test_demo_baidu/testElement.yaml', 'r')
-testElement = yaml.load(testElementFile, Loader=None)
+testElement = yaml.load(testElementFile, Loader=yaml.FullLoader)
 
 
 @pytest.mark.parametrize('inputValue', searchData)
@@ -23,10 +24,12 @@ def test_search(inputValue):
         driver = webdriver.Chrome(config['driverConfig'])
         url = testData['test_url_homepage']
         driver.get(url)
-        WebDriverWait(driver, 3, 0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR, testElement["searchBar"])))
+        webWait(driver, 3, 0.5, testElement["searchBar"])
         driver.find_element_by_css_selector(testElement["searchBar"]).send_keys(inputValue)
-        # WebDriverWait(driver, 5, 0.5).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#content_left')))
-        # content = driver.find_element_by_css_selector('#content_left')
-
+        driver.find_element_by_css_selector(testElement["submitBtn"]).click()
+        WebDriverWait(driver, 5, 0.5).until(EC.presence_of_element_located((By.ID, testElement["targetContent"])))
+        content = driver.find_element_by_id(testElement["targetContent"]).text
+        assert inputValue in content
+        driver.quit()
     except BaseException as e:
         raise e
